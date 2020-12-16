e01c3ac856e04d98e7da6a9a69611892368fc7f3
Pelumi Longe <longe.pelumi@yahoo.com>
2020-12-16 13:39:27 +0100

    make rspec test pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  helper_method :is_admin?
-
   def is_admin?
     session[:is_admin]
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def is_admin?
+    session[:is_admin]
+  end
 end
